b23594b4ccee41d4950c51deed81e7e2e1300c66
LiMee233 <LiMee233@outlook.com>
2024-07-07 20:47:30 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Copilot2ChatGPT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
